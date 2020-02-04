109daacfff1feacb7f68fec66cc02b20522c0a18
Aniruddh Chouksey <aniruddhc@vmware.com>
2020-02-04 13:09:26 +0530

    removing the blockchain mon folder and updating for brown bag presentation

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -56,7 +56,7 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
 Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
 	let nonce = 0;
 	let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-	while(hash.substring(0,5) !== '00000') {
+	while(hash.substring(0,4) !== '0000') {
 		nonce++;
 		hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
 		console.log(nonce + " " + hash);		
